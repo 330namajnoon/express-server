dcc86898ac5a43e127e649951e9a312f78381444
330namajnoon <92964328+330namajnoon@users.noreply.github.com>
2024-01-18 12:21:20 +0100

    Update Server.js

--- Server.js
+++ Server.js
@@ -3,61 +3,38 @@ const http = require("http");
 const path = require("path");
 const cors = require("cors");
 const multer = require("multer");
+const Controller = require("sm-express-server/Controller");
+const context = require("sm-express-server/context");
+const { methodsMap } = require("sm-express-server/constants");
 
 
-function Server(port = 4000, direction = "./", use = [], fileDirection = "") {
-    this.storage = multer.diskStorage({
-        destination: (req, file, cd)=> {
-            cd(null, fileDirection);
-        },
-        filename: (req, file, cd)=> {
-            cd(null, file.originalname);
-        }
-    })
-    this.upload = multer({storage: this.storage});
+function Server(port = 4000, direction = "./", use = []) {
+    /**@type {Controller[]} */
+    this.controllers = [];
     this.port = process.env.PORT || port;
     this.direction = direction;
     this.app = express();
     this.pdp = path.join(__dirname, this.direction);
     this.app.use(cors());
     this.app.use(express.static(this.pdp));
-    use.forEach((u)=> {
+    use.forEach((u) => {
         this.app.use(u);
     })
     this.server = http.createServer(this.app);
-  
+    context.app = this.app;
 }
-Server.prototype.start = function(calback) {
-    this.server.listen(this.port, ()=> {
-        console.log(`server is up on port ${this.port}`);
+Server.prototype.start = function (calback) {
+    this.server.listen(this.port, () => {
+        this.controllers.forEach(c => {
+            this.app[methodsMap[c.method]](c.path, c.storage, c.callback);
+        })
         calback();
-    })
+    });
 }
-Server.prototype.setControls = function(controls = [{method: "GET" || "POST" || "DELETE" || "PUT" || "UPDATE", path: "", action:()=>{}, file: null}]) {
-    this.controls = controls;
-    this.controls.forEach(([method, path, action, file])=> {
-        switch (method) {
-            case "GET":
-                this.app.get(path,multer().none(),(req, res)=> {
-                    action(req, res);
-                })
-                break;
-            case "POST":
-                if (file) {
-                    this.app.post(path,this.upload.single(file) ,(req, res)=> {
-                        action(req, res);
-                    })
-                }else {
-                    this.app.post(path ,multer().none(),(req, res)=> {
-                        action(req, res);
-                    })
-                }
-                break;
-        
-            default:
-                break;
-        }
-    })
+Server.prototype.addControllers = /**@param{Controller[]}controllers */ function (controllers) {
+    controllers.forEach(c => {
+        this.controllers.push(c);
+    });
 }
 
-module.exports = {Server};
+module.exports = { Server };
